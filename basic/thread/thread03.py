91414a0c49bb8c043bddfdf3bf4494fda087cd7b
kqyang <kqyang0000@163.com>
2019-09-19 11:36:56 +0800

    添加注释

--- basic/thread/thread03.py
+++ basic/thread/thread03.py
@@ -1,5 +1,7 @@
 from multiprocessing import Pool
-import os, time, random
+import os
+import time
+import random
 
 
 def long_time_task(name):
@@ -12,10 +14,13 @@ def long_time_task(name):
 
 if __name__ == '__main__':
     print('parent process %s' % os.getpid())
+    # Pool()默认参数为系统cpu核数
     p = Pool(4)
     for i in range(5):
         p.apply_async(long_time_task, args=(i,))
     print('wait for all subProcess done...')
+    # 当调用close()方法时就不能继续添加你的进程了
     p.close()
+    # Pool()的join()方法会等待所有子进程执行结束，并且调用join()之前必须调用close()方法
     p.join()
     print('All subProcess done...')
