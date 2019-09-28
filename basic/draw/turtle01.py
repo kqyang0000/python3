6aef13ae11e80e7ff23d56c8745243616681daaf
kqyang <kqyang0000@163.com>
2019-09-28 11:58:52 +0800

    turtle 图形界面

---
+++ basic/draw/turtle01.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+# 设置笔刷宽度
+width(4)
+# 前进
+forward(200)
+# 右转90度
+right(90)
+# 笔刷颜色
+pencolor('red')
+forward(100)
+right(90)
+pencolor('green')
+forward(200)
+right(90)
+pencolor('blue')
+forward(100)
+right(90)
+
+done()
